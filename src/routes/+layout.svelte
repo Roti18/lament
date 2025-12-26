98e143c859d79a57d0b7c15d3867638b290ccc0d
Roti18 <akuronif@gmail.com>
2025-12-27 00:34:01 +0700

    feat: add root layout component

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import './layout.css';
+	import favicon from '$lib/assets/favicon.svg';
+	import Player from '$lib/components/Player.svelte';
+	import Navigation from '$lib/components/Navigation.svelte';
+	import SearchModal from '$lib/components/SearchModal.svelte';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<title>Lament</title>
+	<meta name="description" content="A calm, personal music listening experience" />
+	<meta name="theme-color" content="#0d0e10" />
+</svelte:head>
+
+<Navigation />
+
+<main class="pt-4 lg:pb-24 lg:pl-24 min-h-screen pb-[calc(10rem+env(safe-area-inset-bottom,0px))]">
+	<div class="max-w-6xl px-4 lg:px-6 mx-auto">
+		{@render children()}
+	</div>
+</main>
+
+<Player />
+
+<SearchModal />
